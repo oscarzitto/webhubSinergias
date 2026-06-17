5da38fdc015b007a020b810405c3e654d58d7851
oscarzitto <oscar.rodrigo2001@hotmail.com>
2026-06-16 22:17:11 -0400

    css y logo

--- webhub-sinergias/src/components/Navbar.jsx
+++ webhub-sinergias/src/components/Navbar.jsx
@@ -1,14 +1,44 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "../styles/navbar.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Inicio</Link> |{" "}
-      <Link to="/participantes">Instituciones Participantes</Link> |{" "}
-      <Link to="/avances">Avances</Link> |{" "}
-      <Link to="/recursos">Recursos</Link> |{" "}
-      <Link to="/contacto">Contacto</Link> |{" "}
-      <Link to="/login">Login - Acceso Privado</Link>
+    <nav className="navbar">
+
+      <Link to="/" className="navbar-logo">
+        <img
+          src={logo}
+          alt="SINERGIAS"
+        />
+      </Link>
+
+      <div className="navbar-links">
+
+        <Link to="/">Inicio</Link>
+
+        <Link to="/participantes">
+          Instituciones Participantes
+        </Link>
+
+        <Link to="/avances">
+          Avances
+        </Link>
+
+        <Link to="/recursos">
+          Recursos
+        </Link>
+
+        <Link to="/contacto">
+          Contacto
+        </Link>
+
+        <Link to="/login">
+          Login - Acceso Privado
+        </Link>
+
+      </div>
+
     </nav>
   );
 }
